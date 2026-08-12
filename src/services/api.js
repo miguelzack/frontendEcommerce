92d87c1cd3bbda68d430dc89df2ccf90fa22b220
miguelzackk <mzsilvaw@gmail.com>
2026-08-12 16:06:40 -0300

    fix authorization header interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("nexo_token");
-  if (token && !config.headers.Authorization) config.headers.Authorization = `Bearer ${token}`;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   else delete config.headers.Authorization;
   return config;
 });
